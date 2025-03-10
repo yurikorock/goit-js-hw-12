5e8662b29899ecb8f971a67d30b83f3c5d78eef5
Yura Shaplavsky <yurikorock777@gmail.com>
2025-03-10 19:43:02 +0200

    rewrite pixabay to async function

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,9 +5,9 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '49133693-4042ac5cb8c0a8ba13ba0f36c';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function getImages(query) {
-  return axios
-    .get('/', {
+export async function getImages(query) {
+  try {
+    const response = await axios.get('/', {
       params: {
         key: API_KEY,
         q: query,
@@ -15,20 +15,32 @@ export function getImages(query) {
         orientation: 'horizontal',
         safesearch: true,
       },
-    })
-    .then(response => {
-      return response.data.hits; // Отримані дані
-    })
-    .catch(error => {
-      console.error('Помилка:', error);
-      return [];
     });
 
-  // return fetch(`${BASE_URL}?client_id=${API_KEY}`)
-  //   .then(resp => {
-  //     console.log(resp);
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //   });
+    return response.data.hits; // Отримані дані
+  } catch (error) {
+    console.error('Помилка:', error);
+    return [];
+  }
 }
+// code before refactoring
+
+// export function getImages(query) {
+//   return axios
+//     .get('/', {
+//       params: {
+//         key: API_KEY,
+//         q: query,
+//         image_type: 'photo',
+//         orientation: 'horizontal',
+//         safesearch: true,
+//       },
+//     })
+//     .then(response => {
+//       return response.data.hits; // Отримані дані
+//     })
+//     .catch(error => {
+//       console.error('Помилка:', error);
+//       return [];
+//     });
+// }
