e92d1e6468eb2cde245facd8723dd48c75983bed
Yura Shaplavsky <yurikorock777@gmail.com>
2025-03-14 18:57:35 +0200

    add window scroll

--- src/main.js
+++ src/main.js
@@ -36,25 +36,24 @@ form.addEventListener('submit', async event => {
   loader.style.display = 'block';
   clearGallery(); //очищаємо галерею перед новим пошуком
   try {
-    getImages(searchQuery).then(images => {
-      if (images.length === 0) {
-        gallery.innerHTML = ''; // Очищаємо галерею якщо масив пустий
+    const images = await getImages(searchQuery);
+    if (images.length === 0) {
+      gallery.innerHTML = ''; // Очищаємо галерею якщо масив пустий
 
-        iziToast.show({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          messageColor: 'white',
-          backgroundColor: '#ef4040',
-          position: 'topRight',
-          class: 'custom-toast',
-        });
+      iziToast.show({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        messageColor: 'white',
+        backgroundColor: '#ef4040',
+        position: 'topRight',
+        class: 'custom-toast',
+      });
 
-        return;
-      }
-      renderGallery(images);
+      return;
+    }
+    renderGallery(images);
 
-      btnLoadMore.classList.remove('is-hidden');
-    });
+    btnLoadMore.classList.remove('is-hidden');
   } catch (error) {
     console.log(error);
   } finally {
@@ -75,6 +74,8 @@ async function onLoadMore() {
       return;
     }
     renderGallery(images);
+    scrollPageAfterLoad(); // функція яка буде скролити галерею при
+    //нажатті кнопки завантажити ще
 
     if (images.length < 15) {
       btnLoadMore.classList.add('is-hidden');
@@ -84,5 +85,20 @@ async function onLoadMore() {
     console.log(error);
   } finally {
     loader.style.display = 'none';
+  }
+}
+
+function scrollPageAfterLoad() {
+  const galleryItems = document.querySelectorAll('.gallery-item');
+  if (galleryItems.length > 0) {
+    //беремо останній елемент
+    const lastItem = galleryItems[galleryItems.length - 1];
+    //висота однієї карточки галереї
+    const itemHeight = lastItem.getBoundingClientRect().height;
+    // плавна прокрутка
+    window.scrollBy({
+      top: itemHeight * 2,
+      behavior: 'smooth',
+    });
   }
 }
